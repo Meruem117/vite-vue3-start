54b7b95fa13d978d19eab2acc13c3056d4acc265
Meruem117 <1572677319@qq.com>
2021-11-14 22:52:39 +0800

    update user

--- src/components/main/myspace/Mine.vue
+++ src/components/main/myspace/Mine.vue
@@ -1,24 +1,28 @@
 <template>
     <div
-        class="flex flex-col justify-start space-y-5 pl-16 pt-6 w-full h-full text-2xl tracking-wide"
+        v-if="store.state.isLogin"
+        class="flex flex-col justify-start space-y-5 pl-16 pt-6 w-full h-full tracking-wide"
     >
-        <div class="-ml-6">个人资料</div>
+        <div class="-ml-6 text-xl">个人资料</div>
         <div class="flex">
-            <div class="column">用户名:</div>
-            <div class="inline">{{ user.name }}</div>
+            <div class="column-name">用户名:</div>
+            <div class="column-value">{{ user.name }}</div>
         </div>
         <div class="flex">
-            <div class="column">性别:</div>
-            <div class="inline">{{ user.gender }}</div>
+            <div class="column-name">性别:</div>
+            <div class="column-value">{{ user.gender }}</div>
         </div>
         <div class="flex">
-            <div class="column">地区:</div>
-            <div class="inline">{{ user.location }}</div>
+            <div class="column-name">地区:</div>
+            <div class="column-value">{{ user.location }}</div>
         </div>
         <div class="flex">
-            <div class="column">生日:</div>
-            <div class="inline">{{ user.birthday }}</div>
+            <div class="column-name">生日:</div>
+            <div class="column-value">{{ user.birthday }}</div>
         </div>
+    </div>
+    <div v-else class="flex justify-center pt-4">
+        <div class="column-name">暂未登录</div>
     </div>
 </template>
 
@@ -31,9 +35,13 @@ const { user } = store.state
 </script>
 
 <style scoped>
-.column {
-    font-size: 18px;
-    color: rgba(156, 163, 175);
+.column-name {
+    font-size: 17px;
+    color: gray;
     width: 1/6;
+}
+.column-value {
+    display: inline;
+    font-size: 18px;
 }
 </style>
