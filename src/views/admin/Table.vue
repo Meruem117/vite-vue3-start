65faba76e926c05c42ceecd40f20390a46a7589f
Meruem117 <1572677319@qq.com>
2021-11-19 20:24:25 +0800

    update table

--- src/views/admin/Table.vue
+++ src/views/admin/Table.vue
@@ -1,10 +1,20 @@
 <template>
     <div class="adminContent flex-col">
-        <h1>Tables</h1>
-        <UpTable />
+        <a-tabs type="card" v-model:activeKey="activeKey" size="large">
+            <a-tab-pane key="1" tab="Up">
+                <UpTable />
+            </a-tab-pane>
+            <a-tab-pane key="2" tab="User">
+                <UserTable />
+            </a-tab-pane>
+        </a-tabs>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import UpTable from '@/components/admin/table/UpTable.vue'
+import UserTable from '@/components/admin/table/UserTable.vue'
+
+const activeKey = ref('1')
 </script>
