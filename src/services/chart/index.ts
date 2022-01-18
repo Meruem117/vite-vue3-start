b2dfdb026a05da5e20808446356a161910d4cfa1
Meruem117 <1572677319@qq.com>
2022-01-19 00:11:56 +0800

    update chart service

--- src/services/chart/index.ts
+++ src/services/chart/index.ts
@@ -1,11 +1,6 @@
 import axios from 'axios'
 import { mResultItem, hResultItem1, hResultItem2, hResultItem3, hResultItem4 } from '@/models/chart'
 
-export async function getMResult(): Promise<mResultItem[]> {
-  const response = await axios.get('/api/chart/getMResult')
-  return response.data
-}
-
 export async function getHResult1(): Promise<hResultItem1[]> {
   const response = await axios.get('/api/chart/getHResult1')
   return response.data
@@ -23,5 +18,30 @@ export async function getHResult3(): Promise<hResultItem3[]> {
 
 export async function getHResult4(): Promise<hResultItem4[]> {
   const response = await axios.get('/api/chart/getHResult4')
+  return response.data
+}
+
+export async function getMResult(): Promise<mResultItem[]> {
+  const response = await axios.get('/api/chart/getMResult')
+  return response.data
+}
+
+export async function getDistinctTm(): Promise<string[]> {
+  const response = await axios.get('/api/chart/getDistinctTm')
+  return response.data
+}
+
+export async function getMResultByType(type: number): Promise<mResultItem[]> {
+  const response = await axios.get(`/api/chart/getMResultByType?type=${type}`)
+  return response.data
+}
+
+export async function getMResultByTypeAndTm(type: number, tm: string): Promise<mResultItem[]> {
+  const response = await axios.get('/api/chart/getMResultByTypeAndTm', { params: { type, tm } })
+  return response.data
+}
+
+export async function getMResultByNameAndTm(name: string, tm: string): Promise<mResultItem> {
+  const response = await axios.get('/api/chart/getMResultByNameAndTm', { params: { name, tm } })
   return response.data
 }
